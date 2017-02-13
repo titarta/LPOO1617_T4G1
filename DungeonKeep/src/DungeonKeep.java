8a10a40a5057c5c5ddefe3bae4c4aea57d09990f
titarta <tiagorvcarvalho@gmail.com>
2017-02-13 16:44:45 +0000

    Store and draw map.

---
+++ DungeonKeep/src/DungeonKeep.java
@@ -0,0 +1,26 @@
+
+public class DungeonKeep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char map[][] = {
+				{'X','X','X','X','X','X','X','X','X','X'},
+				{'X','H',' ',' ','I',' ','X',' ','G','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','X','X',' ','X','X','X','X',' ','X'},
+				{'X',' ','I',' ','I',' ','X','k',' ','X'},
+				{'X','X','X','X','X','X','X','X','X','X'}
+				};
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println(map[i]);
+		}
+		
+		
+	}
+
+}
